74727a83bbbd516cca84ff8d29b271fa07423508
guenyoo <guenay@gmail.com>
2020-10-18 17:02:48 +0200

    Added Lockabl Logo and LogIn to Router

---
+++ src/components/LogIn.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="c-login">
+    LogIn Component
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/LogIn.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login">
+    <h1 class="login__headline"><strong>Welcome</strong> to</h1>
+    <img
+      src="@/assets/lockable_logo.png"
+      alt="Welcome to Lockabl"
+      class="login__logo"
+    >
+    <LogIn msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LogIn from '@/components/LogIn.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    LogIn,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+  &__headline {
+    font-size: 18px;
+    font-weight: 400;
+    margin-bottom: 0;
+  }
+
+  &__logo {
+    max-width: 230px;
+  }
+}
+</style>
