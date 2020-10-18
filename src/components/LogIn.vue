018f971b0f988c1dbf56ecd7329cf8c9f04a9ce1
guenyoo <guenay@gmail.com>
2020-10-18 17:09:33 +0200

    Created base component Input

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="c-login">
-    LogIn Component
+    <p class="c-login__label">
+      Please <strong>log in</strong> to your account
+    </p>
+    <Input
+      type="text"
+      placeholder="My E-Mail address"
+    />
   </div>
 </template>
 
 <script>
-export default {
+import Input from './base/Input.vue';
 
+export default {
+  components: {
+    Input,
+  },
 };
 </script>
 
---
+++ src/components/base/Input.vue
@@ -0,0 +1,25 @@
+<template>
+  <input
+    :type=type
+    :placeholder=placeholder
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    placeholder: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
