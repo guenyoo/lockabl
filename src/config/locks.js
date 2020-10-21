bc5d497a6031db008be6c4a104ad828190f80e3a
guenyoo <guenay@gmail.com>
2020-10-21 12:09:46 +0200

    Added first lock to config file

---
+++ src/config/locks.js
@@ -0,0 +1,15 @@
+const LOCKS = [
+  {
+    name: 'Bicycle',
+    connectivity: '77%',
+    img: false,
+    color: 'blue',
+    status: 'locked',
+    favorite: false,
+    sharedWith: [],
+  },
+];
+
+export {
+  LOCKS,
+};
