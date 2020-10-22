b80ecfc05e7a3e0bb71080dd61384ad30180a5ce
guenyoo <guenay@gmail.com>
2020-10-22 13:06:31 +0200

    Added content wrapper to LogOut view

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
-    <Back />
-    <Logout />
+    <Header />
+    <div class="u-content">
+      <Back />
+      <Logout />
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Back from '@/components/Back.vue';
 import Logout from '@/components/Logout.vue';
+import Footer from '@/components/Footer.vue';
+import Header from '@/components/Header/Header.vue';
 
 export default {
   components: {
     Back,
     Logout,
+    Header,
+    Footer,
   },
 };
 </script>
